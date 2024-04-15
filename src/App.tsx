b8b3052f97f94eb9c6592ac9935af1444d83e8bb
mehrabix <mehrabi@post.com>
2024-04-15 21:38:25 +0330

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      <Button>Click me</Button>
+    </>
+  )
+}
+
+export default App
